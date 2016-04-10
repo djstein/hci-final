837efb92874552795e49fde027779dbb14308e0f
Dylan Stein <djstein@ncsu.edu>
2016-04-10 17:58:08 -0400

    User Create Form, test user added

--- hci/rockclimb/urls.py
+++ hci/rockclimb/urls.py
@@ -5,7 +5,8 @@ from django.contrib.auth.views import logout, login
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='home'),
 
-    # Account
+    # Account login, logout, register account
     url(r'^accounts/login/$', login, name='login'),
     url(r'^accounts/logout/$', logout, {'next_page': "/"}, name="logout"),
+    url(r'^user/accounts/register/$', RegisterView.as_view(), name='user'),
 ]
--- hci/rockclimb/views.py
+++ hci/rockclimb/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth import authenticate, login
+
+from forms import *
 
 
 # Create your views here.
@@ -11,3 +15,36 @@ class HomePageView(TemplateView):
         template_name (str): Template to be rendered
     """
     template_name = 'rockclimb/home.html'
+
+
+class RegisterView(CreateView):
+    """CreateView for User
+
+    Attributes:
+        form_class (UserCreateForm) = form class that creates a User
+        model (User) = User object to create
+        success_message (str) = Message upon form valid
+        success_url (str) = Reverse to FeedView
+        template_name (str): Generic template to be rendered
+    """
+    form_class = UserCreateForm
+    model = User
+    success_message = "Account created successfully!"
+    success_url = reverse_lazy('home')
+    template_name = 'registration/register.html'
+
+    def form_valid(self, form):
+        """
+        If the form is valid, redirect to the supplied URL.
+
+        This function also creates the user then logs them in
+        """
+        response =  super(RegisterView, self).form_valid(form)
+
+        # Get the username and password created, log the user
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+
+        return response
