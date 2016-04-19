9d558bc68c502214fcb8211cac02ed9caf091f92
bjschwa2 <bjschwa2@ncsu.edu>
2016-04-19 18:20:00 -0400

    changed location of procfile

--- hci/hci/settings.py
+++ hci/hci/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'rockclimb/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
